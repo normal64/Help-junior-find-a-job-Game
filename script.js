826e677704de8ce2996c3e3c6ff66d650ce366e5
Denis Poplavskii <normal64@mail.ru>
2021-10-31 20:43:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const character = document.getElementById("character");
+const block = document.getElementById("block");
+let counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
+}
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
+        alert("Go learn more css,html,js, react, master jquery,api and greek language.  Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+}, 10);
